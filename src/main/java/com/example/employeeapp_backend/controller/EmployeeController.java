80d7d6e1ad7b0691a5dcf3bbfed81242350a0734
roshmaep <roshmaep1999@gmail.com>
2022-12-06 10:27:46 +0530

    add

--- src/main/java/com/example/employeeapp_backend/controller/EmployeeController.java
+++ src/main/java/com/example/employeeapp_backend/controller/EmployeeController.java
@@ -1,34 +1,20 @@
 package com.example.employeeapp_backend.controller;
 
+import com.example.employeeapp_backend.model.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
-    @PostMapping("/")
-    public String Welcomepage(){
-        return "welcome to employee page";
+
+    @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
+    public String EmployeeAdd(@RequestBody Employee e)
+    {
+        System.out.println(e.getName().toString());
+        return "Employee added succesfully";
     }
-    @PostMapping("/add")
-    public String Employeeadd(){
-        return "Employee add";
-    }
-    @PostMapping("/search")
-    public String Employeesearch(){
-        return "Employee Search";
-}
-    @PostMapping("/edit")
-    public String Employeeedit(){
-        return "Employee Edit";
-    }
-    @GetMapping("/viewall")
-    public String Employeeview(){
-        return "View all employee";
-    }
-    @PostMapping("/delete")
-    public String Employeedelete(){
-        return "Employee delete";
-    }
+
 }
