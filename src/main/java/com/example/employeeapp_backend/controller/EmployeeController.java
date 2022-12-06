c022b6d05e04d8cf1d6850e77e63dad578161d25
roshmaep <roshmaep1999@gmail.com>
2022-12-06 10:36:54 +0530

    add values

--- src/main/java/com/example/employeeapp_backend/controller/EmployeeController.java
+++ src/main/java/com/example/employeeapp_backend/controller/EmployeeController.java
@@ -13,7 +13,15 @@ public class EmployeeController {
     @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
     public String EmployeeAdd(@RequestBody Employee e)
     {
+
         System.out.println(e.getName().toString());
+        System.out.println(e.getCode());
+        System.out.println(e.getCmpname().toString());
+        System.out.println(e.getDesignation().toString());
+        System.out.println(e.getMobileno().toString());
+        System.out.println(e.getPassword().toString());
+        System.out.println(e.getSalary().toString());
+        System.out.println(e.getUsername().toString());
         return "Employee added succesfully";
     }
 
